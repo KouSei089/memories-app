2cfa144b7d968ed6d1baae01ca03a3559e4930c9
KouSei089 <qq00891829@icloud.com>
2021-02-13 08:16:44 +0900

    VuetifyDL

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,22 +1,37 @@
 <template>
-  <div id="app">
-    <p>{{ message }}</p>
-  </div>
+  <v-app id="app">
+    <v-toolbar color="red lighten-3" dark>
+      <v-toolbar-title>Snippet App</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn flat>New Snippet</v-btn>
+    </v-toolbar>
+    <!--<p>{{ memorys }}</p>-->
+  </v-app>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: function () {
     return {
-      message: "Hello Vue!"
+      memorys: "memorys"
+    }
+  },
+  mounted() {
+    this.setMemorys();
+  },
+  methods: {
+    setMemorys: function() {
+      axios.get('/api/memorys')
+      .then(response => (
+        this.memorys = response.data
+      ))
     }
   }
 }
 </script>
 
 <style scoped>
-p {
-  font-size: 2em;
-  text-align: center;
-}
+
 </style>
