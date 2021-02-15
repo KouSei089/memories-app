21b657642c02cc3c6c1df9a9d30cff719ce7f651
KouSei089 <qq00891829@icloud.com>
2021-02-15 09:44:53 +0900

    enviromentSet

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -1,17 +1,19 @@
-// This file is automatically compiled by Webpack, along with any other files
-// present in this directory. You're encouraged to place your actual application logic in
-// a relevant structure within app/javascript and only use these pack files to reference
-// that code so it'll be compiled.
+import Vue from 'vue'
+import Vuetify from "vuetify"
+import "vuetify/dist/vuetify.min.css"
+import App from '../App.vue'
+import Router from '../router/router.js'
 
-require("@rails/ujs").start()
-require("turbolinks").start()
-require("@rails/activestorage").start()
-require("channels")
+Vue.use(Vuetify); 
+const vuetify = new Vuetify(); 
 
+document.addEventListener('DOMContentLoaded', () => {
+  const app = new Vue({
+    vuetify,
+    router: Router,
+    render: h => h(App)
+  }).$mount()
+  document.body.appendChild(app.$el)
 
-// Uncomment to copy all static images under ../images to the output folder and reference
-// them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
-// or the `imagePath` JavaScript helper below.
-//
-// const images = require.context('../images', true)
-// const imagePath = (name) => images(name, true)
+  console.log(app)
+})
