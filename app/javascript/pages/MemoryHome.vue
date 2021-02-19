02ecb25625b61bd48605a4350a18a65e737fbd89
KouSei089 <qq00891829@icloud.com>
2021-02-19 21:58:10 +0900

    vue-youtubeFalse

--- app/javascript/pages/MemoryHome.vue
+++ app/javascript/pages/MemoryHome.vue
@@ -5,51 +5,37 @@
         <v-layout>
           <v-flex xs6>
             <!-- HTML あとから修正 -->
-            <p>Youtube</p>
+            <div>
+              <youtube :video-id="url"></youtube>
+            </div>
           </v-flex>
           <v-flex xs6>
-            <v-card>
-              <v-card-title>{{ memory.title }}</v-card-title>
-              <v-card-subtitle>{{ memory.emotion }}</v-card-subtitle>
-              <v-card-text>{{ memory.description }}</v-card-text>
-              <v-card-actions>
-                <v-btn text color="red"><router-link :to="{ path: '/edit/' + memory.id }">Update</router-link></v-btn>
-                <v-btn text color="gray" @click="deleteMemory(memory.id)">Delete</v-btn>
-              </v-card-actions>
-            </v-card>
+            <v-card-title>{{ memory.title }}</v-card-title>
+            <v-card-subtitle>{{ memory.emotion }}</v-card-subtitle>
+            <v-card-text>{{ memory.description }}</v-card-text>
+            <v-card-actions>
+              <v-btn text color="red"><router-link :to="{ path: '/edit/' + memory.id }">Update</router-link></v-btn>
+              <v-btn text color="gray" @click="deleteMemory(memory.id)">Delete</v-btn>
+            </v-card-actions>
           </v-flex>
 
         </v-layout>
       </v-card>
-     
-
-      <!-- deleteポップアップ 
-      <v-dialog v-model="dialogDelete" width="400">
-          <v-card>
-            <v-card-title class="headline red lighten-3 white--text" primary-title>
-              Confirm
-            </v-card-title>
-            <v-spacer></v-spacer>
-            <v-card-text>
-              <p>本当に削除してもよろしいですか？</p>
-            </v-card-text>
-
-            <v-divider></v-divider>
-
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="red" @click="deleteMemory(memory.id)">
-                Delete
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog> -->
-
   </v-container>
 </template>
 
 <script>
 import axios from 'axios';
+import VueYoutube from 'vue-youtube'
+import Vue from 'vue'
+
+/*var video_id = window.location.search.split('v=')[1];
+var ampersandPosition = video_id.indexOf('&');
+if(ampersandPosition != -1) {
+  video_id = video_id.substring(0, ampersandPosition);
+} */
+
+Vue.use(VueYoutube)
 
 export default {
   name: 'MemoryHome',
@@ -57,6 +43,7 @@ export default {
     return {
       memorys: "memorys",
       dialogDelete: false,
+      url: "Wdll9P9icJU"
     }
   },
   mounted: function() {
@@ -70,22 +57,12 @@ export default {
         console.log(error);
       });
     },
-    /*setMemoryInfo(id) {
-      axios.get('/api/memorys/' + id + '.json').then(res => {
-        this.memoryEd = res.data;
-     });
-    },*/
     deleteMemory(id) {
       axios.delete('/api/memorys/' + id).then(res => {
         this.memorys = "memorys";
         this.fetchMemorys();
       });
-      //this.dialogDelete = !this.dialogDelete
     },
-    /*deleteModel(id) {
-      this.id = id
-      this.dialogDelete = !this.dialogDelete
-    }*/
   }
 }
 </script>
