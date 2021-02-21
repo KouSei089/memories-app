4f5c04b862d0b41190f3190f15ecec694cd65329
KouSei089 <qq00891829@icloud.com>
2021-02-21 11:55:17 +0900

    検索機能完了

--- app/javascript/pages/MemoryHome.vue
+++ app/javascript/pages/MemoryHome.vue
@@ -1,25 +1,74 @@
 <template>
   <!-- main -->
   <v-container style="height: 1000px; max-width: 2400px;">
-      <v-card v-for="memory in memorys" :key="memory.id" style="margin-top: 20px; margin-bottom: 20px;">
-        <v-layout>
-          <v-flex xs6>
-            <!-- HTML あとから修正 -->
-            <div>
-              <iframe :src="memory.youtubeurl | embedVideo" allow="fullscreen"></iframe>
-            </div>
-          </v-flex>
-          <v-flex xs6>
-            <v-card-title>{{ memory.title }}</v-card-title>
-            <v-card-subtitle>{{ memory.emotion }}</v-card-subtitle>
-            <v-card-text>{{ memory.description }}</v-card-text>
+
+    <v-text-field
+      v-model="keyword"
+    ></v-text-field>
+
+    <!--<v-text-field 
+      v-model="searchWord" 
+      v-on:keyup="abstruct" 
+      label="Input Keyword" 
+      style='margin-top:4px'>
+    </v-text-field>
+
+    <v-autocomplete
+      v-model="value"
+      :items="memorys"
+      item-text="emotion"
+      label="Emotion Select"
+      >
+    </v-autocomplete> -->
+   
+    <v-row>
+      <v-col
+        v-for="memory in filteredMemorys" 
+        :key="memory.id"
+        cols="6"
+        md="4"
+      >
+        <v-card>
+          <div>
+            <iframe :src="memory.youtubeurl | embedVideo" allow="fullscreen"
+            width="100%" height="235px"
+            frameborder="0" style="border: none"
+            ></iframe>
+          </div>
+          <v-card-title>{{ memory.title }}</v-card-title>
+          <v-card-subtitle>{{ memory.emotion }}</v-card-subtitle>
+          <v-card-text class="text--primary">{{ memory.description }}</v-card-text>
+          <v-card-actions>
+            <v-btn text>
+              <!-- カラー変更 -->
+              <router-link :to="{ path: '/edit/' + memory.id }">Update</router-link>
+            </v-btn>
+          <!-- 削除機能 -->
+            <v-btn text color="pink" @click="toggleDelete(memory.id)">Delete
+            </v-btn>  
+          </v-card-actions>
+        </v-card>
+
+        <v-dialog v-model="dialogDelete" width="400">
+          <v-card color="blue-grey darken-4">
+            <v-spacer></v-spacer>
+            <v-card-title></v-card-title>
+            <v-card-text>
+              <p style="color:white;">本当に削除してもよろしいですか？</p>
+            </v-card-text>
+            <v-divider></v-divider>
             <v-card-actions>
-              <v-btn text color="red"><router-link :to="{ path: '/edit/' + memory.id }">Update</router-link></v-btn>
-              <v-btn text color="gray" @click="deleteMemory(memory.id)">Delete</v-btn>
+              <v-spacer></v-spacer>
+              <v-btn text color="pink" @click="deleteMemory()">
+                Completely deleted
+              </v-btn>
             </v-card-actions>
-          </v-flex>
-        </v-layout>
-      </v-card>
+          </v-card>
+        </v-dialog>
+
+      </v-col>
+    </v-row>
+
   </v-container>
 </template>
 
@@ -30,28 +79,57 @@ export default {
   name: 'MemoryHome',
   data: function () {
     return {
-      memorys: "memorys",
-      dialogDelete: false,
-      //url: "https://www.youtube.com/watch?v=ghSp6p3IZwI"
+      dialogDelete: false, 
+      keyword: "",
+      memorys: null,
     }
   },
   mounted: function() {
     this.fetchMemorys();
   },
+  computed: {
+    filteredMemorys: function() {
+      return this.filterMemorys();
+    }
+  },
   methods: {
     fetchMemorys() {
       axios.get('/api/memorys').then((res) => {
-        this.memorys = res.data 
+        this.memorys  = res.data 
       }, (error) => {
         console.log(error);
       });
     },
-    deleteMemory(id) {
-      axios.delete('/api/memorys/' + id).then(res => {
-        this.memorys = "memorys";
-        this.fetchMemorys();
-      });
+    filterMemorys: function() {
+      let filtered = [];
+      for (let i in this.memorys) {
+        let memory = this.memorys[i];
+        if (memory.title.indexOf(this.keyword) !== -1) {
+          filtered.push(memory);
+        }
+      }
+      return filtered;
     },
+    deleteMemory: function() {
+      axios.delete('/api/memorys/' + this.id + '.json')
+        .then(response => {
+          this.fetchMemorys();
+        }
+      );
+      this.dialogDelete = !this.dialogDelete
+    },
+    toggleDelete: function(id) {
+      this.id = id
+      this.dialogDelete = !this.dialogDelete
+    },
+    /*abstruct: function() {
+      this.memorys = []
+      for (i=0;i<this.allData.length;i++) {
+        if((this.allData[i].emotion.indexOf(this.searchWord) !== -1) || (this.allData[i].title.indexOf(this.searchWord) !== -1) || (this.allData[i].description.indexOf(this.searchWord) !== -1)){
+           this.memorys.push(this.allData[i])
+        }
+      }
+    }*/
   },
   filters: {
     embedVideo(url) {
@@ -62,3 +140,10 @@ export default {
   },
 }
 </script>
+
+<style scoped>
+a {
+  color: white;
+  text-decoration: none;
+}
+</style>
