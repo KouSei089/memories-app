1bc8abb71fd28415bdafac65ab8c20e3da5259bc
KouSei089 <qq00891829@icloud.com>
2021-03-01 12:30:57 +0900

    AddPassword

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,14 @@
-5.times do
-  Memory.create(
-    title: Faker::Lorem.word,
-    emotion: Faker::Lorem.word,
-    description: Faker::Lorem.sentence,
-  )
-end
+#5.times do
+  #Memory.create(
+    #title: Faker::Lorem.word,
+    #emotion: Faker::Lorem.word,
+    #description: Faker::Lorem.sentence,
+  #)
+#end
 
 10.times do
   User.create(
-    name: Faker::Pokemon.name,
+    name: Faker::Lorem.word,
     email: Faker::Internet.email,
   )
 end
